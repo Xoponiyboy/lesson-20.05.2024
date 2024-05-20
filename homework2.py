4bdd39b14eeeccb79807ce775e1d35723a668299
Xoponiyboy <ilyailyanov@yandex.ru>
2024-05-20 19:16:38 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+quantity_ready_homework = '12'
+quantity_hour = '1.5'
+name_course = 'Python'
+time_of_task = float(quantity_hour) / float(quantity_ready_homework)
+print(name_course + ', всего задач ' + quantity_ready_homework + ', затрачено часов: ' + quantity_hour + ', среднее время выполнения: ' + str(time_of_task))
